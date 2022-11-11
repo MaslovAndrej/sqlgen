8179b2224e318f42372efeed44106095a18716be
Andrey <maslov.andrej@gmail.com>
2022-11-11 21:47:11 +0300

    Down to netstandard 2.0

--- SQLQueryGen/Adapter/Sqlite.cs
+++ SQLQueryGen/Adapter/Sqlite.cs
@@ -14,6 +14,55 @@ namespace SQLQueryGen.Adapter
             return $"Data Source={filePath}";
         }
 
+        public string GetCreateQuery(string table)
+        {
+            return string.IsNullOrEmpty(Schema) ?
+                $"CREATE TABLE IF NOT EXISTS {table}" :
+                $"CREATE TABLE IF NOT EXISTS {Schema}.{table}";
+        }
+
+        public string GetInsertQuery(string table)
+        {
+            return string.IsNullOrEmpty(Schema) ?
+                $"INSERT INTO {table}" :
+                $"INSERT INTO {Schema}.{table}";
+        }
+
+        public string GetUpdateQuery(string table)
+        {
+            return string.IsNullOrEmpty(Schema) ?
+                $"UPDATE {table}" :
+                $"UPDATE {Schema}.{table}";
+        }
+
+        public string GetDeleteQuery(string table)
+        {
+            return string.IsNullOrEmpty(Schema) ?
+                $"DELETE FROM {table}" :
+                $"DELETE FROM {Schema}.{table}";
+        }
+
+        public string GetSelectQueryFromElement(string table, string alias)
+        {
+            return string.IsNullOrEmpty(Schema) ?
+                $"FROM {table} {alias}" :
+                $"FROM {Schema}.{table} {alias}";
+        }
+
+        public string GetSelectQueryJoinElement(string type, string table, string fieldJoin, string fieldMain, string aliasJoin, string aliasMain)
+        {
+            return string.IsNullOrEmpty(Schema) ?
+                $"{type} {table} {aliasJoin} ON {aliasJoin}.{fieldJoin} = {aliasMain}.{fieldMain}" :
+                $"{type} {Schema}.{table} {aliasJoin} ON {aliasJoin}.{fieldJoin} = {aliasMain}.{fieldMain}";
+        }
+
+        public string GetDropQuery(string type, string name)
+        {
+            return string.IsNullOrEmpty(Schema) ?
+                $"DROP {type} IF EXISTS {name}" :
+                $"DROP {type} IF EXISTS {Schema}.{name}";
+        }
+
         public string GetKeyFieldType()
         {
             return "INTEGER PRIMARY KEY";
--- SQLQueryGen/Database.cs
+++ SQLQueryGen/Database.cs
@@ -4,86 +4,20 @@ namespace SQLQueryGen
 {
     public interface IDatabase
     {
-        public string ConnectionString { get; }
-        public string Schema { get; set; }
+        string ConnectionString { get; }
+        string Schema { get; set; }
 
-        public virtual string GetConnectionString()
-        {
-            return string.Empty;
-        }
-
-        public string GetCreateQuery(string table)
-        {
-            return string.IsNullOrEmpty(Schema) ?
-                $"CREATE TABLE IF NOT EXISTS {table}" :
-                $"CREATE TABLE IF NOT EXISTS {Schema}.{table}";
-        }
-
-        public string GetInsertQuery(string table)
-        {
-            return string.IsNullOrEmpty(Schema) ?
-                $"INSERT INTO {table}" :
-                $"INSERT INTO {Schema}.{table}";
-        }
-
-        public string GetUpdateQuery(string table)
-        {
-            return string.IsNullOrEmpty(Schema) ?
-                $"UPDATE {table}" :
-                $"UPDATE {Schema}.{table}";
-        }
-
-        public string GetDeleteQuery(string table)
-        {
-            return string.IsNullOrEmpty(Schema) ?
-                $"DELETE FROM {table}" :
-                $"DELETE FROM {Schema}.{table}";
-        }
-
-        public string GetSelectQueryFromElement(string table, string alias)
-        {
-            return string.IsNullOrEmpty(Schema) ?
-                $"FROM {table} {alias}" :
-                $"FROM {Schema}.{table} {alias}";
-        }
-
-        public string GetSelectQueryJoinElement(string type, string table, string fieldJoin, string fieldMain, string aliasJoin, string aliasMain)
-        {
-            return string.IsNullOrEmpty(Schema) ?
-                $"{type} {table} {aliasJoin} ON {aliasJoin}.{fieldJoin} = {aliasMain}.{fieldMain}" :
-                $"{type} {Schema}.{table} {aliasJoin} ON {aliasJoin}.{fieldJoin} = {aliasMain}.{fieldMain}";
-        }
-
-        public string GetDropQuery(string type, string name)
-        {
-            return string.IsNullOrEmpty(Schema) ?
-                $"DROP {type} IF EXISTS {name}" :
-                $"DROP {type} IF EXISTS {Schema}.{name}";
-        }
-
-        public string GetKeyFieldType()
-        {
-            return string.Empty;
-        }
-
-        public string GetNavigateFieldType()
-        {
-            return string.Empty;
-        }
-
-        public virtual string GetFieldType(Type propertyType)
-        {
-            return string.Empty;
-        }
-
-        public virtual string GetFieldType(Type propertyType, int size)
-        {
-            return string.Empty;
-        }
-
-        public virtual string GetFieldValue(Type propertyType, object value)
-        {
-            return string.Empty;
-        }
+        string GetCreateQuery(string table);
+        string GetInsertQuery(string table);
+        string GetUpdateQuery(string table);
+        string GetDeleteQuery(string table);
+        string GetSelectQueryFromElement(string table, string alias);
+        string GetSelectQueryJoinElement(string type, string table, string fieldJoin, string fieldMain, string aliasJoin, string aliasMain);
+        string GetDropQuery(string type, string name);
+        string GetKeyFieldType();
+        string GetNavigateFieldType();
+        string GetFieldType(Type propertyType);
+        string GetFieldType(Type propertyType, int size);
+        string GetFieldValue(Type propertyType, object value);
     }
 }
